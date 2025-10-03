f6dd9a812d6e6818f4bb14cacc383ebe7eabe862
Lucas Trunce <lucastrunce@icloud.com>
2025-10-03 09:28:13 +0100

    fix?

---
+++ scraper/rules.mjs
@@ -0,0 +1,349 @@
+export default {
+    badWords: [
+        "us based",
+        "onsite sf",
+        "remote us",
+        "us-only",
+        "us only",
+        "north america only",
+        "remote us",
+        "austin, tx",
+        "remote canada",
+        "remote india",
+        "us-based remote",
+        "us time zones only",
+        "us remote",
+        "us resident",
+        "must be a us",
+        "us timezone",
+        "remote us-based",
+        "remote in canada",
+        "us only",
+        "canada only",
+        "us-remote",
+        "usa pacific time",
+        "remote in usa",
+        "remote uk",
+        ".net",
+        "c#",
+        "remote from the united states",
+        "f#",
+        "remote u.s.",
+        "u.s. time zones required",
+        "clojurescript",
+        "est timezone",
+        "bay area",
+        "sf bay",
+        "usa | remote",
+    ],
+    scoreWords: [
+        { word: "profitable", score: 50 },
+        { word: "bootstrapped", score: 50 },
+        { word: "revenue", score: 50 },
+        { word: 'revenue', score: 20 },
+        {
+            word: 'remote-first',
+            score: 10,
+        },
+        {
+            word: 'us/india',
+            score: -100,
+        },
+        {
+            word: 'london, uk',
+            score: -70,
+        },
+        {
+            word: 'fully-remote',
+            score: 50,
+        },
+        {
+            word: 'zurich',
+            score: 70,
+        },
+        {
+            word: 'switzerland',
+            score: 70,
+        },
+        {
+            word: 'next',
+            score: 10,
+        },
+        {
+            word: 'creative',
+            score: 10,
+        },
+        {
+            word: 'open-minded',
+            score: 10,
+        },
+        {
+            word: 'test',
+            score: 10,
+        },
+        {
+            word: 'trunk',
+            score: 10,
+        },
+        {
+            word: 'c#',
+            score: -10,
+        },
+        {
+            word: "€",
+            score: 50,
+        },
+        {
+            word: "$",
+            score: 25,
+        },
+        {
+            word: "central european",
+            score: 5,
+        },
+        {
+            word: "europe",
+            score: 5,
+        },
+        {
+            word: "cet",
+            score: 5,
+        },
+        {
+            word: "kafka",
+            score: 10,
+        },
+        {
+            word: "k8s",
+            score: 10,
+        },
+        {
+            word: "react",
+            score: 20,
+        },
+        {
+            word: "node.js",
+            score: 10,
+        },
+        {
+            word: "next.js",
+            score: 40,
+        },
+        {
+            word: 'utc',
+            score: 5,
+        },
+        {
+            word: "typescript",
+            score: 100,
+        },
+        {
+            word: "postgres",
+            score: 30,
+        },
+        {
+            word: 'php',
+            score: -100,
+        },
+        {
+            word: "graphql",
+            score: 20,
+        },
+        {
+            word: "remote only",
+            score: 50,
+        },
+        {
+            word: "remote ok",
+            score: 10,
+        },
+        {
+            word: "salary",
+            score: 20,
+        },
+        {
+            word: "webassembly",
+            score: 10,
+        },
+        {
+            word: "golang",
+            score: -20,
+        },
+        {
+            word: "react native",
+            score: -20,
+        },
+        {
+            word: 'fully remote',
+            score: 50,
+        },
+        {
+            word: "several roles",
+            score: 50,
+        },
+        {
+            word: 'multiple roles',
+            score: 50,
+        },
+        {
+            word: 'multiple locations',
+            score: 50,
+        },
+        {
+            word: "remote company",
+            score: 10,
+        },
+        {
+            word: "remote",
+            score: 30,
+        },
+        {
+            word: 'hybrid',
+            score: -20,
+        },
+        {
+            word: 'on-site',
+            score: -30,
+        },
+        {
+            word: 'onsite',
+            score: -30,
+        },
+        {
+            word: "remote anywhere",
+            score: 15,
+        },
+        {
+            word: "remote (worldwide",
+            score: 5,
+        },
+        {
+            word: "fully remote",
+            score: 10,
+        },
+        {
+            word: "series a",
+            score: 5,
+        },
+        {
+            word: "mysql",
+            score: -10,
+        },
+        {
+            word: "redux",
+            score: -10,
+        },
+        {
+            word: "blockchain",
+            score: -150,
+        },
+        {
+            word: "smart contracts",
+            score: -100,
+        },
+        {
+            word: "ror",
+            score: -50,
+        },
+        {
+            word: "rails",
+            score: -50,
+        },
+        {
+            word: "frontend engineer",
+            score: 10,
+        },
+        {
+            word: "full-stack engineer",
+            score: 10,
+        },
+        {
+            word: "backend engineer",
+            score: 10,
+        },
+        {
+            word: "product engineer",
+            score: 10,
+        },
+        {
+            word: "ruby",
+            score: -50,
+        },
+        {
+            word: "python",
+            score: -10,
+        },
+        {
+            word: "java",
+            score: -70,
+        },
+        {
+            word: "kotlin",
+            score: -15,
+        },
+        {
+            word: "crypto",
+            score: -100,
+        },
+        {
+            word: "nft",
+            score: -100,
+        },
+        {
+            word: "defi",
+            score: -100,
+        },
+        {
+            word: "web3",
+            score: -100,
+        },
+        {
+            word: 'pst',
+            score: -100,
+        },
+        {
+            word: 'est',
+            score: -100,
+        },
+        {
+            word: "remote us only",
+            score: -100,
+        },
+        {
+            word: "mongo",
+            score: -10,
+        },
+        {
+            word: "remote eu",
+            score: 100,
+        },
+        {
+            word: 'tanstack',
+            score: 10,
+        },
+        {
+            word: "in-person",
+            score: -10,
+        },
+        {
+            word: 'remote global',
+            score: 50,
+        },
+        {
+            word: "remote within eu",
+            score: 100,
+        },
+        {
+            word: "remote within uk",
+            score: -100,
+        },
+        {
+            word: "remote within us",
+            score: -100,
+        },
+        {
+            word: "remote within canada",
+            score: -100,
+        },
+    ],
+};
